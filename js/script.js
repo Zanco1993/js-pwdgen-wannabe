8762d0b0b9828e889a12c48a347de25f2d050d5e
Zanco1993 <zanconatofabio@gmail.com>
2021-12-02 15:24:24 +0100

    valutazione iniziale sulla logica da seguire

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* dichiariamo le variabili e richiediamo all'utente
+ Nome, Cognome, Colore preferito
+*/
+
+
+/*
+il nome ed il cognome voglio che siano univoci quindi
+gli inserirò come costante (const)
+
+mentre al colore preferito, voglio dare la possibilità di essere cambiato in futuro
+quindi lo inserisco dentro ad una varibile (let) 
+*/
+
